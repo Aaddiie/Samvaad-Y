e69fcbd5c6382721349e55001c39d77ad4ae9b06
Aditiya Shanker Shukla <aditiya080904@gmail.com>
2026-04-09 12:06:07 +0530

    fix: moved bgImage to public and removed import

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -14,7 +14,7 @@ import send_button from './send_button.svg'
 import menu_icon from './menu_icon.png'
 import arrow_icon from './arrow_icon.png'
 import code from './code.svg'
-const bgImage = "/bgImage.svg";
+const bgImage = "/bgimage.svg";
 import pic1 from './pic1.png'
 import pic2 from './pic2.png'
 import pic3 from './pic3.png'
